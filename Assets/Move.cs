0368e9276d7ec80393709e19d1c94463f9aa6416
Émilie Parent <lolabff@hotmail.com>
2023-04-20 10:20:30 -0400

    Controllable jump force

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -7,6 +7,7 @@ public class Move : MonoBehaviour
     private Animator anim;
     private Rigidbody2D rb;
     private bool isJumping = false;
+    private float jumpForce = 15f;
 
     public float speed = 10f;
     public float jumpAmount = 10;
@@ -47,11 +48,26 @@ public class Move : MonoBehaviour
         if (input_x == 0 && input_y == 0)
             anim.SetInteger("direction", 0);
 
+
         if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow))&& !isJumping)
         {
-            rb.AddForce(new Vector2(0, jumpAmount), ForceMode2D.Impulse); //use raycast
+            InvokeRepeating("CalculateJumpForce", 0f, 0.2f);
+        }
+
+        if ((Input.GetKeyUp(KeyCode.Space) || Input.GetKeyUp(KeyCode.UpArrow)) && !isJumping)
+        {
+            CancelInvoke();
+            if (jumpForce > 35f)
+                jumpForce = 35f;
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse); //use raycast
+            jumpForce= 15f;
         }
 
         //rb.MovePosition(rb.position + new Vector2(input_x, 0) * Time.deltaTime * speed);
+    }
+
+    public void CalculateJumpForce()
+    {
+        jumpForce += 10;
     }
 }
