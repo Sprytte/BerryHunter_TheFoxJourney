2d922095f1d4d839fc12302ff00b3ef6c5b4a184
Émilie Parent <lolabff@hotmail.com>
2023-05-14 18:39:58 -0400

    ahh

--- Assets/EndScript.cs
+++ Assets/EndScript.cs
@@ -6,8 +6,6 @@ using UnityEngine.SceneManagement;
 
 public class EndScript : MonoBehaviour
 {
-    [SerializeField]
-    private AudioSource btnclicksfx;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,32 +19,14 @@ public class EndScript : MonoBehaviour
     }
     public void loadMain()
     {
-        btnclicksfx.Play();
-        StartCoroutine(delayLoadingMain());
+        Debug.Log("click btn");
+        SceneManager.LoadScene("SampleScene");
         GameObject fox = GameObject.Find("Fox");
         fox.transform.position = new Vector3(1, -3, 0);
     }
     public void quitApp()
     {
-        btnclicksfx.Play();
         Application.Quit();
 
     }
-    public void loadCredits()
-    {
-        btnclicksfx.Play();
-        StartCoroutine(delayLoadingCreds());
-        
-    }
-    private IEnumerator delayLoadingMain()
-    {   
-        yield return new WaitForSeconds(3f);
-        SceneManager.LoadScene("SampleScene");
-    }
-    private IEnumerator delayLoadingCreds()
-    {   
-        yield return new WaitForSeconds(3f);
-        SceneManager.LoadScene("CreditsScene");
-    }
-   
 }
